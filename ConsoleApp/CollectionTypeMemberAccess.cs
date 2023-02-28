fa213aa388845b0b190ecc41ac963f774c40c683
mehdi etehadi <m.etehadi@outlook.com>
2023-02-28 23:24:47 +0100

    Add DirectAssign and CollectionTypeMemberAccess bench marks

---
+++ ConsoleApp/CollectionTypeMemberAccess.cs
@@ -0,0 +1,39 @@
+﻿//| Method          | Mean          | Error         | StdDev        | Allocated |
+//| --------------- | ----------:   | ----------:   | ----------:   | ----------:|
+//| ReadList        | 0.2146 ns     | 0.0227 ns     | 0.0212 ns     | -         |
+//| ReadArray       | 0.0485 ns     | 0.0059 ns     | 0.0055 ns     | -         |
+//| ReadDictionary  | 4.2243 ns     | 0.0789 ns     | 0.0700 ns     | -         |
+
+using BenchmarkDotNet.Attributes;
+
+
+[MemoryDiagnoser]
+public class CollectionTypeMemberAccess
+{
+    List<int> li = new(1000);
+    Dictionary<int, int> di = new(1000);
+    int[] arr = new int[1000];
+
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            li.Add(i);
+            di.Add(i, i);
+            arr[i] = i;
+        }
+    }
+
+    [Benchmark]
+    public int ReadList() => li[500];
+
+
+    [Benchmark]
+    public int ReadArray() => arr[500];
+
+
+    [Benchmark]
+    public int ReadDictionary() => di[500];
+}
---
+++ ConsoleApp/DirectAssign.cs
@@ -0,0 +1,90 @@
+﻿//| Method | Mean | Error | StdDev | Median | Allocated |
+//| ---------------------- | ---------:| --------:| ---------:| ---------:| ----------:|
+//| AssignProp | 134.2 ns | 3.83 ns | 11.30 ns | 126.8 ns | - |
+//| FieldProp | 149.0 ns | 0.05 ns | 0.04 ns | 149.0 ns | - |
+//| AssignPropDrivedClass | 134.5 ns | 3.49 ns | 10.28 ns | 127.7 ns | - |
+//| FieldPropDrivedClass | 149.0 ns | 0.10 ns | 0.09 ns | 149.0 ns | - |
+
+
+
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser]
+public class DirectAssign
+{
+    public class Test
+    {
+        public string StringProp { get; set; }
+        public string StringField;
+    }
+
+
+    public class TestBase
+    { 
+        public string StringProp { get; set; }
+        public string StringField;
+    }
+
+    public class TestDrivedClass : TestBase
+    { 
+
+    }
+
+    Test test;
+    TestDrivedClass drivedTest;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    { 
+        test = new Test();
+        drivedTest = new TestDrivedClass();
+    }
+
+    [Benchmark]
+    public void AssignLocalVariable()
+    {
+        string name;
+        for (int i = 0; i < 100; i++)
+        {
+            name = "Value";
+        }
+    }
+
+    [Benchmark]
+    public void AssignProp()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            test.StringProp = "Value";
+        }
+    }
+
+
+    [Benchmark]
+    public void FieldProp()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            test.StringField = "Value";
+        }
+    }
+
+    [Benchmark]
+    public void AssignPropDrivedClass()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            drivedTest.StringProp = "Value";
+        }
+    }
+
+
+    [Benchmark]
+    public void FieldPropDrivedClass()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            drivedTest.StringField = "Value";
+        }
+    }
+}
